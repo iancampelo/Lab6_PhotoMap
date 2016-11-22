cd52ec337c7f19615fb451e5f758e41f783e8072
Ian Campelo <iancampelo@gmail.com>
2016-11-22 05:38:00 -0200

    Initial commit

---
+++ Photo Map/PhotoMapViewController.swift
@@ -0,0 +1,70 @@
+//
+//  PhotoMapViewController.swift
+//  Photo Map
+//
+//  Created by Nicholas Aiwazian on 10/15/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
+                                              MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func onCameraClick(_ sender: UIButton) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        
+        if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)) {
+            vc.sourceType = UIImagePickerControllerSourceType.camera
+        } else {
+            vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        }
+        
+        
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        print("Info original -> \(originalImage)")
+        print("Info edited -> \(editedImage)")
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onBtnClick(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "tagSegue", sender: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
